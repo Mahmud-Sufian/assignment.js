1ce1cebc264037a9a6f4dff24103d6d8c40a4958
Mahmud-Sufian <mahmud5281585@gmail.com>
2021-01-22 15:20:54 +0600

    creating problem solve in js

---
+++ assignment.js
@@ -0,0 +1,64 @@
+
+
+
+//kilometerToMeter
+function kilometerToMeter(kilometer){
+    var meter = 1000;
+    var meter = kilometer * meter;
+    return meter;
+}
+
+
+
+
+
+// budgetCalculator
+function budgetCalculator(clock, mobile, laptop){
+    var totalClock = 50 * clock;
+    var totalMobile = 100 * mobile;
+    var totalLaptop = 500 * laptop;
+    var totalPrice = totalClock + totalMobile + totalLaptop;
+    return totalPrice;
+}
+
+
+
+// hotelCost
+function hotelCost(day){
+    var rent = 0;
+    if(day <= 10){
+        rent = day * 100;
+    }
+    else if(day <= 20){
+        var firstTenDay = 1000;
+        var remaining = day - 10;
+        var secondTenDay = remaining * 80;
+        rent = firstTenDay + secondTenDay;
+    }
+    else{
+        var firstTenDay = 1000;
+        var secondTenDay = 800;
+        var remaining = day - 20;
+        var afterTwenty = remaining * 50;
+        rent = firstTenDay + secondTenDay + afterTwenty;
+    }
+    return rent;
+}
+console.log(hotelCost(22));
+
+
+
+// megaFriend
+ function megaFriend(friends) {
+     var larger = friends[0];
+
+    for (var i = 0; i < friends.length; i++) {
+        var char = friends[i];
+
+        if (char.length > larger.length) {
+            larger = char;
+        }
+    }
+    return larger;
+}
+
